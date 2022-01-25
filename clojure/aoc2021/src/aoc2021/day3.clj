647574669cc22983e845ddab19d6ea25557ab8d7
phoenix2082 <jr2082@protonmail.com>
2022-01-25 18:48:07 +0530

    Advent Of code - Solution to day 3, part 2.

--- clojure/aoc2021/src/aoc2021/day3.clj
+++ clojure/aoc2021/src/aoc2021/day3.clj
@@ -13,7 +13,13 @@
 (defn convert-lst-to-bin [lst]
   (map #(convert-string-to-bin %) lst))
 
-(defn counter-lst [hmap]
+(defn counter-lst
+  " Takes a map of bin value and list and returns a map with count.
+  Example
+  Input: {[0 [0 0 0 0]], [1, [1,1,1]]}
+  Output {:0 4, :1 3}
+  "
+  [hmap]
   (let [[f1 s1] (first hmap)
         [f2 s2] (second hmap)]
     (-> (assoc {} (keyword (str f1)) (count s1))
@@ -36,7 +42,16 @@
            0
            1)) bin-map))
 
-(defn bin-list-to-decimal [lst]
+(defn bin-list-to-decimal
+  "Reverses the list of binary digits and
+  convert it to decimal value.
+
+  Ex. For binary number (1 1 1 1 1 1 1 1 1 0 0 1)
+
+  > (bin-list-to-decimal '(1 1 1 1 1 1 1 1 1 0 0 1))
+    4089
+  "
+  [lst]
   (->> (map-indexed (fn [idx itm] (* (Math/pow 2 idx) itm))
                     (reverse lst))
        (apply +)))
@@ -51,5 +66,80 @@
         erate (-> (epsilon-rate bin-lst)
                   (bin-list-to-decimal))]
     (* grate erate)))
-    
+
+;;;; Part 2 solution.
+(defn filter-by-idx-binval
+  "Select only those values for which element
+  at index `idx` has `binval` in list `lst`.
+
+  Example:
+  (filter-by-idx-binval 0 1 [(0 1 1 0) (1 0 0 1) (1 1 0 1)])
+   Returns - Only those list which has at index 0, binary 1
+  [(1 0 0 1)]
+  "
+  [idx binval lst]
+  (filter #(= (nth % idx) binval) lst))
+
+(defn most-common-value [kv]
+  (cond
+    (> (:1 kv) (:0 kv)) 1
+    (> (:0 kv) (:1 kv)) 0
+    :else 1))
+
+(defn least-common-value [kv]
+  (cond
+    (< (:1 kv) (:0 kv)) 1
+    (< (:0 kv) (:1 kv)) 0
+    :else 0))
+
+(defn filter-list [lst idx rating-fn]
+  (as-> (apply map vector lst) ilist
+    (group-by identity (nth ilist idx))
+    (counter-lst ilist)
+    (rating-fn ilist)
+    (filter-by-idx-binval idx ilist lst)))
+
+(defn find-rating
+  "Find rating recursively from list of ratings,
+  filtering entries by rating function.
+
+  Ex:
+  To Filter by most common value in bit position.
+  (find-rating lst most-common-value)
+
+  Or, To Filter by least common value in current bit position.
+
+  (find-rating lst least-common-value)
+
+  where lst is list of `list of binary` value e.x.
+  
+  [(1 0 1 0 0 1) (0 1 0 1 1 0) (1 0 1 0 10).....]
+
+  IMP: least-common-value is function.
+
+  Returns: List of binary value
+  ((0 1 0 1 1 0))
+  
+  "
+  [lst rating-fn]
+  (loop [n 0
+         result lst]
+    (if (= 1 (count result))
+      result
+      (recur (inc n) (filter-list result n rating-fn)))))
+
+(defn calculate-rating
+  "Calculate decimal rating by filtering list,
+  and using rating function."
+  [lst rating-fn]
+  (-> (find-rating lst rating-fn)
+      (first)
+      (bin-list-to-decimal)))
+
+(defn day3-part2-ans [filepath]
+  (let [inp (-> (read-input filepath)
+                (convert-lst-to-bin))
+        ox-rating (calculate-rating inp most-common-value)
+        co2-rating (calculate-rating inp least-common-value)]
+    (* ox-rating co2-rating)))
       
